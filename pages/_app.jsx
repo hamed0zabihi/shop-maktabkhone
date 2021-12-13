ee249abafe1391eb1c691a724d83ddb68db5e166
hamed zabihi <hamed0zabihi@gmail.com>
2021-12-13 17:01:40 +0330

    :zap: add redux structure

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope */
 import React from 'react'
+import { Provider } from 'react-redux'
+import { Store } from './../redux/store/Store'
 import { ThemeProvider } from '@emotion/react'
 import Layout from '../components/Styles/Layout/Layout'
 import theme from '../configs/theme'
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </ThemeProvider>
+  <Provider store={Store}>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  </Provider>
 )
 
 export default MyApp
